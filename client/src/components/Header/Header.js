cc3ca0cca24a62fe4fcce6383e19b31d715b39db
Tim Lolkema <timlolkema@Tims-MacBook-Air.local>
2021-09-17 14:52:52 +0200

    express-react-cypress initial commit

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -4,6 +4,6 @@ import logo from '../../HeaderImage.png';
 export default class Header extends React.Component {
 
   render() {
-    return <img src={logo} id="Header" className="Header"></img>;  
+    return <img src={logo} id="Header" className="Header" alt="Cypress" ></img>;  
   }
 }
